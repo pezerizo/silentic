601f90e9a7d53fba0efa55537a5dd7e93468b97b
pezerizo <pezerizo@email.com>
2023-08-16 20:38:56 +0200

    SessionManager.c startServer function added

--- src/SessionManager.c
+++ src/SessionManager.c
@@ -1,9 +1,12 @@
-#pragma once
+#ifndef _S_INET_C_
+#define _S_INET_C_
+
 #include "..\include\SessionManager.h"
 
 
+
 int initWSA(PSILENTSERVER server){
-    if (WSAStartup(MAKEWORD(2, 2), &(server->wsa)) != 0){
+    if (WSAStartup(MAKEWORD(2, 2), server->wsa) != 0){
         #ifdef _DEBUG_
             printf("\tWSA init errorr\n");
         #endif
@@ -21,21 +24,23 @@ PSILENTSERVER initServer(){
 }
 
 
-PSILENTSESSION initSession(PSILENTSERVER server, uint16_t port = 55550, unsigned long from_ip = INADDR_ANY){
+PSILENTSESSION initSession(PSILENTSERVER server, uint16_t port, uint16_t buffer_size){
     PSILENTSESSION newSession = (PSILENTSESSION)calloc(1, sizeof(SILENTSESSION));
     if (memset(newSession, 0x0, sizeof(SILENTSESSION)) != newSession) return NULL;
 
-    for(size_t iter = 0, size_t miss = 0; iter < MAXSESSCOUNT; ++iter){
+    size_t miss = 0
+    for(size_t iter = 0; iter < MAXSESSCOUNT; ++iter){
         if(server->session_container[iter] == NULL){
 
             newSession->session_addr.sin_family = AF_INET;
-            newSession->session_addr.sin_addr.s_addr = from_ip;
-            newSession->session_addr.sin_port = port;
+            newSession->session_addr.sin_addr.s_addr = INADDR_ANY;
+            newSession->session_addr.sin_port = htons(port);
             newSession->id = iter;
+            newSession->port = port;
+            newSession->buffer = (uint8_t*)calloc(1, buffer_size);
             newSession->session_socket = socket(AF_INET, SOCK_STREAM, 0);
-            bind(newSession->session_socket, (struct sockaddr*)&(newSession->session_addr), sizeof(struct sockaddr));
-            listen(newSession->session_socket, 2);
 
+            bind(newSession->session_socket, (struct sockaddr*)&(newSession->session_addr), sizeof(struct sockaddr));
 
             server->session_container[iter] = newSession;
             server->session_in_container++;
@@ -46,7 +51,7 @@ PSILENTSESSION initSession(PSILENTSERVER server, uint16_t port = 55550, unsigned
 
             server->session_in_poll++;
 
-
+            listen(newSession->session_socket, 1);
             break;
         }else{
             if(++miss == MAXSESSCOUNT) return NULL;
@@ -55,3 +60,23 @@ PSILENTSESSION initSession(PSILENTSERVER server, uint16_t port = 55550, unsigned
 
     return NULL;
 }
+
+void startServer(PSILENTSERVER server){
+    while(1){
+        for(size_t iter = 0; iter < MAXSESSCOUNT; ++iter){
+            if((server->session_container[iter]->session_socket != 0 &&
+                server->session_poll[iter].fd != NULL &&
+                server->session_container[iter]->client_addr == 0) && (server->session_poll[iter].revents & POLLIN)){
+                    server->session_container[iter]->client_socket = accept(server->session_container[iter]->session_socket, (struct sockaddr *)&(server->session_container[iter]->client_addr), sizeof(struct sockaddr));
+                    printf("new session created with %s", inet_ntoa(server->session_container[iter]->client_addr.sin_addr))
+                }
+            else if((server->session_container[iter]->session_socket != 0 &&
+                server->session_poll[iter].fd != NULL &&
+                server->session_container[iter]->client_addr == 0) && (server->session_poll[iter].revents & POLLIN)){
+                    printf("something to read from %s", inet_ntoa(server->session_container[iter]->client_addr.sin_addr))
+                }
+        }
+    }
+}
+
+#endif
