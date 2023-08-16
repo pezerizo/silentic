cd28fcb35b1a1b86c5f812c3209cb86223097526
pezerizo <pezerizo@email.com>
2023-08-16 20:39:07 +0200

    changed

--- src/main.c
+++ src/main.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "..\include\session_manager.h"
-#include "..\include\structs_manager.h"
+#include <winsock2.h>
+#include "SessionManager.c"
 
 int main(){
-    struct _sic_server_data* server = calloc(1, sizeof(struct _sic_server_data));
-    struct _sic_session_data* client = NULL;
+    PSILENTSERVER silentic = initServer();
+    initWSA(silentic);
+    initSession(silentic, 55550, 8192);
 
-    server->port = 55555;
-    server->conn_allowed = 1;
-    server->conn_established = 0;
-    server->sess_items = 0;
-
-
-    sicInitWSA(server);
-    createSocketTCPWin(server);
-    bindSocketWin(server);
-    listenSocketWin(server);
-    //client = initSession(server, client);
-    acceptSocketWin(server, client);
-
-
-    freeSessionList(server, client);
-    freeServer(server);
-    sicDestroyWSA();
 
     return 0;
 }
